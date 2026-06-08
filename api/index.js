a5cb8d6a1a99cb956944966cf8d6b563a4c67bbd
adamimir <adamibnu02@gmail.com>
2026-06-08 10:24:46 +0800

    fix2

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
 import app from '../server/app.js';
 
-// Vercel serverless functions expect a `(req, res)` handler.
-// `app` is an Express application and can be invoked directly.
-export default function handler(req, res) {
-	return app(req, res)
-}
+export default app;
